9579298e18db89717647460b67c6eb34153068a5
Mufliha Dawood <ms2176@hw.ac.uk>
2023-11-03 09:26:41 +0400

    part 2

---
+++ Week4/main.js
@@ -0,0 +1,9 @@
+var myCity = {
+    "name": "Dubai",
+    "country": "UAE",
+    "place": "Dubai Mall"
+    };
+
+    console.log(myCity.name); // Outputs: Dubai
+    console.log(myCity.country); // Outputs: UAE
+    
