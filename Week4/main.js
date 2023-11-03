5bc87fde8d405950a14e11f6ab9d5e29a562b77c
Mufliha Dawood <ms2176@hw.ac.uk>
2023-11-03 09:35:29 +0400

    renderHTML

--- Week4/main.js
+++ Week4/main.js
@@ -2,8 +2,45 @@ var myCity = {
     "name": "Dubai",
     "country": "UAE",
     "place": "Dubai Mall"
-    };
+};
 
-    console.log(myCity.name); // Outputs: Dubai
-    console.log(myCity.country); // Outputs: UAE
-    
+console.log(myCity.name); // Outputs: Dubai
+console.log(myCity.country); // Outputs: UAE
+
+var myCity = ["Dubai", "UAE", "Dubai Mall"];
+
+console.log(myCity[0]); // Outputs: Dubai
+
+var myCities = [
+    {
+        "name": "Dubai",
+        "country": "UAE",
+        "place": "Dubai Mall"
+    },
+    {
+        "name": "Milan",
+        "country": "Italy",
+        "place": "Duomo Di Milano"
+    }
+];
+
+console.log(myCities[1].country); // Outputs: Italy
+
+//Ajax request
+
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://ms2176.github.io/cities1.json');
+ourRequest.onload = function() {
+var ourData = JSON.parse(ourRequest.responseText);
+renderHTML(ourData);
+};
+ourRequest.send();
+});
+function renderHTML(data){
+var htmlString = "this is a test";
+cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
+
