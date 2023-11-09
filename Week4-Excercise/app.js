cce286aaf0319b164d45f9b113b6bdca21751425
Mufliha Dawood <ms2176@hw.ac.uk>
2023-11-09 15:05:01 +0400

    event listener

--- Week4-Excercise/app.js
+++ Week4-Excercise/app.js
@@ -1,3 +1,12 @@
 const cityInput = document.getElementById("cityInput");
 const weatherInfo = document.getElementById("weather-info");
 const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    const city = cityInput.value;
+   
+    if (!city) {
+        alert("Please enter a city");
+        return;
+    }
+});
