cd8bddd3eab05c005e376b08d27015619d2a48ae
Mufliha Dawood <ms2176@hw.ac.uk>
2023-11-24 22:20:47 +0400

    commit

--- Lab5/nodejs/routes/pages.js
+++ Lab5/nodejs/routes/pages.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const router = express.Router();
+authController = require('../controllers/auth');
 
-router.post('/', (req, res) => {
-    res.render('index');
-});
+router.get ("/",(req,res) => {
+    res.render("index",{
+       
+    });
+    
+    });
+router.get ("/register",(req,res) => {
+    res.render("register");
+        
+    });
+router.get ("/login",(req,res) => {
+    res.render("login");
+            
+    });
 
-router.post('/register', (req, res) => {
-    res.render('register');
-});
+router.get ("/profile",(req,res) => {
+    const user = {
+        id: req.query.id,
+        name: req.query.name,
+        email: req.query.email
+    };
+    res.render('profile',{user});
+                
+    });
 
 module.exports = router;
